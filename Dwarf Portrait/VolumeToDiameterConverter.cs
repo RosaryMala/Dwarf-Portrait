11c7f82bf0e46884270115996c3aa5d2b0552e88
Japa <japa.mala.illo@gmail.com>
2016-06-27 10:11:36 +0530

    corrected the volume calculations.

--- Dwarf Portrait/VolumeToDiameterConverter.cs
+++ Dwarf Portrait/VolumeToDiameterConverter.cs
@@ -17,14 +17,19 @@ namespace Dwarf_Portrait
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is double)
-                return (int)(Math.Pow(((double)value), 3.0) * Math.PI / 6.0);
+                return (int)ConvertBack((double)value);
             else
                 return value;
         }
 
         public static double Convert(double value)
         {
-            return Math.Pow(value * 10 * 6 / Math.PI, 1.0 / 3.0);
+            return Math.Pow(6.0 / Math.PI, 1.0 / 3.0) * Math.Pow(value, 1.0 / 3.0);
+        }
+
+        public static double ConvertBack(double value)
+        {
+            return Math.Pow(value, 3) * Math.PI / 6.0;
         }
     }
 }

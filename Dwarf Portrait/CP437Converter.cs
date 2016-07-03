c273a1f91df3e6a573a22089616a62d7c60b3b23
Japa <japa.mala.illo@gmail.com>
2016-07-03 15:24:44 +0530

    Show creature tile.

---
+++ Dwarf Portrait/CP437Converter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Dwarf_Portrait
+{
+    class CP437Converter : IValueConverter
+    {
+        static Encoding encoding = Encoding.GetEncoding(437);
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is byte)
+            {
+                return Convert(new byte[] { (byte)value });
+            }
+            else if(value is int)
+            {
+                return Convert(new byte[] { (byte)(int)value });
+            }
+            else
+                return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return encoding.GetBytes((string)value);
+        }
+
+        static string Convert(byte[] bytes)
+        {
+            return encoding.GetString(bytes);
+        }
+    }
+}
